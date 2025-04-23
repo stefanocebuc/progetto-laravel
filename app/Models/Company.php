2a52ae4453409410c901e014b29972e1a6b8051c
stefanocebuc <stefano.cebuc@gmail.com>
2025-04-23 22:10:18 +0200

    added new things to  models

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,6 +3,8 @@
 namespace App\Models; // definisce la gerarchia del file 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Drug;
 
 class Company extends Model
 {
@@ -10,4 +12,9 @@ class Company extends Model
     protected $fillable = ['name'];
     //array vuoto se vogliamo consentire la modifica di tutti gli attributi
     // protected $garded = ["id", "created_at", "updated_at"]; -> 
+
+    public function drugs(): HasMany
+    {
+        return $this->hasMany(Drug::class);
+    }
 }
